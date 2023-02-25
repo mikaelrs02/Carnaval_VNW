fb83b5ecc67eedc35def46814315651ef757bd56
Mikael Ribeiro <mikaelribeirors@gmail.com>
2023-02-24 22:03:32 -0300

    Comentario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import bruxa from "./img/bruxa.png";
+import fada from "./img/fada.png";
+import fantasma from "./img/fantasma.png";
+import zumbi from "./img/zumbi.png";
+import pirata from "./img/pirata.png";
+import vampiro from "./img/vampiro.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Carnaval extends Component {
+  state = {
+    fantasias: [
+      { imagem: `${bruxa}`, titulo: 'Bruxa' },
+      { imagem: `${fantasma}`, titulo: 'Fantasma' },
+      { imagem: `${vampiro}`, titulo: 'Vampiro' },
+      { imagem: `${zumbi}`, titulo: 'Zumbi' },
+      { imagem: `${fada}`, titulo: 'Fada' },
+      { imagem: `${pirata}`, titulo: 'Pirata' }
+    ]
+  };
+  render() {
+    return (
+      <>
+        <h1>Carnaval VNW</h1>
+        <section className="main">
+          {this.state.fantasias.map((item) => (
+            <div className="fantasia">
+              <img src={item.imagem} alt={item.titulo} />
+              <h2>{item.titulo}</h2>
+            </div>
+          ))}
+        </section>
+        <section className="footer">
+          {this.state.fantasias.map((item) => (
+            <div className="fantasiaFooter">
+              <img src={item.imagem} />
+            </div>
+          ))}
+        </section>
+      </>
+
+    );
+  }
 }
 
-export default App;
+
+export default Carnaval;
+
+
