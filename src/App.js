cf231a6431daabdd51e02a6539e52ed7d2966ccf
Mikael Ribeiro <mikaelribeirors@gmail.com>
2023-03-09 19:22:23 -0300

    [200~last modified

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class Carnaval extends Component {
   render() {
     return (
       <>
-        <h1>Carnaval VNW</h1>
+        <div className="carnavalVnw"><h1>Carnaval VNW</h1></div>
         <section className="main">
           {this.state.fantasias.map((item) => (
             <div className="fantasia">
